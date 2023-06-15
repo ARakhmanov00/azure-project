c4ae89ce6793fbdc45996b79b0519201f8c7fe4f
ARakhmanov00 <abdulazizrakhmanov@yahoo.com>
2023-06-15 20:16:23 +0000

    r53

--- route_53.tf
+++ route_53.tf
@@ -1,12 +1,12 @@
 provider "aws" {
     region = "us-east-2"
-    access_key=#add you access key
-    secret_key=#add your secret key
+    access_key= "AKIA3HSBUZG4TOBQARDZ"
+    secret_key= "YeV5BDsEpe8+iIzwZ4sZDco4wssXdCCzDZm5Tqnl"
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = #add zone ID
-  name    = #add name
+resource "aws_route53_record" "wordpress" {
+  zone_id = "Z10280403S2MA8C5S7FIA"
+  name    = "blog.devops00.com"
   type    = "A"
   ttl     = 300
   records = [azurerm_public_ip.wordpress.ip_address]

07c9adc4a61f5a4925d7743874f343b099ede2f8
ARakhmanov00 <abdulazizrakhmanov@yahoo.com>
2023-06-15 21:27:28 +0000

    l

--- Output.tf
+++ Output.tf
@@ -1,3 +1,14 @@
 output "application_public_address" {
   value = azurerm_public_ip.wordpress.fqdn
 }
+output "scale_set_id" {
+  value = azurerm_linux_virtual_machine_scale_set.wordpress.id
+}
+
+output "scale_set_public_ips" {
+  value = azurerm_linux_virtual_machine_scale_set.wordpress.public_ip_addresses
+}
+
+output "route53_record" {
+  value = aws_route53_record.wordpress
+}

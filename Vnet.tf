afaca73cc24fe4e10c78ceb44a8a05c45578fa85
ARakhmanov00 <abdulazizrakhmanov@yahoo.com>
2023-06-15 20:46:26 +0000

    12

--- Vnet.tf
+++ Vnet.tf
@@ -6,22 +6,8 @@ resource "azurerm_virtual_network" "wordpress" {
   tags                = var.tags
 }
 
-resource "azurerm_subnet" "public1" {
-  name                 = "public1"
-  resource_group_name  = azurerm_resource_group.wordpress.name
-  virtual_network_name = azurerm_virtual_network.wordpress.name
-  address_prefixes     = var.subnet_address
-}
-
-resource "azurerm_subnet" "public2" {
-  name                 = "public2"
-  resource_group_name  = azurerm_resource_group.wordpress.name
-  virtual_network_name = azurerm_virtual_network.wordpress.name
-  address_prefixes     = var.subnet_address
-}
-
-resource "azurerm_subnet" "public3" {
-  name                 = "public3"
+resource "azurerm_subnet" "wordpress" {
+  name                 = "wordpress-subnet"
   resource_group_name  = azurerm_resource_group.wordpress.name
   virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = var.subnet_address

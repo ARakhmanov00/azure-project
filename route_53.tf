ff24f61d975f0cbad76c497df24c940db4ed3f3a
ARakhmanov00 <abdulazizrakhmanov@yahoo.com>
2023-06-20 19:00:27 +0000

    clean

--- route_53.tf
+++ route_53.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
+}
+
 resource "aws_route53_record" "wordpress" {
   zone_id = "Z10280403S2MA8C5S7FIA"
   name    = "blog.devops00.com"
